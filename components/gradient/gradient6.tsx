fbf5541231cc0c89981c9ed4012aaafea4ebc138
Nindi <nindyk4@gmail.com>
2023-03-28 10:08:00 +0700

    fix: fix build error

--- components/gradient/gradient6.tsx
+++ components/gradient/gradient6.tsx
@@ -2,32 +2,15 @@ import React from 'react'
 
 export default function gradient6() {
   return (
-    <div className="absolute -inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-[50px] sm:-top-20">
-    <svg
-      className="relative left-[calc(50%-11rem)] -z-10 h-[90.1875rem] max-w-none -translate-x-1/2 rotate-[0deg] sm:left-[calc(30%-30rem)] sm:h-[75.375rem] lg:mt-[2000px] md:mt-[2490px] mt-[3200px]"
-      viewBox="0 0 1155 678"
-    >
-      <path
-        d="M106 109L1533.5 950.5"
-        stroke="url(#paint0_linear_1354_93351)"
-        stroke-opacity="0.5"
-        stroke-width="20"
-      />
-      <defs>
-        <linearGradient
-          id="paint0_linear_1354_93351"
-          x1="819.75"
-          y1="109"
-          x2="819.75"
-          y2="950.5"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#1DC0AD" />
-          <stop offset="1" stop-color="#1DC0AD" stop-opacity="0" />
-        </linearGradient>
-      </defs>
-    </svg>
-  </div>
-
-  )
+<div className="absolute -inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-[50px] sm:-top-20">
+  <svg className="relative left-[calc(50%-11rem)] -z-10 h-[90.1875rem] max-w-none -translate-x-1/2 rotate-[0deg] sm:left-[calc(30%-30rem)] sm:h-[75.375rem] lg:mt-[2000px] md:mt-[2490px] mt-[3200px]" viewBox="0 0 1155 678">
+    <path d="M106 109L1533.5 950.5" stroke="url(#paint0_linear_1354_93351)" strokeOpacity="0.5" strokeWidth={20} />
+    <defs>
+      <linearGradient id="paint0_linear_1354_93351" x1="819.75" y1={109} x2="819.75" y2="950.5" gradientUnits="userSpaceOnUse">
+        <stop stopColor="#1DC0AD" />
+        <stop offset={1} stopColor="#1DC0AD" stopOpacity={0} />
+      </linearGradient>
+    </defs>
+  </svg>
+</div>  )
 }
